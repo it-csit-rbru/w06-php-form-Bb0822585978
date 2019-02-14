c58053edfe257c2ecd2c934cf9de73805b16a076
Chaichana.K <46773469+Bb0822585978@users.noreply.github.com>
2019-02-14 15:46:28 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <<!DOCTYPE html>
-<html>
+<html lang=en>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>php-6015261004-w05</title>
+    <title>php-6015261004-w06</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
